924652e5b5865baf7fa9fa851ec72ed6987993ec
lsdvitamin <lsdvitamin@yandex.ru>
2018-12-16 14:57:05 +0300

    Блаблабла

---
+++ 1.py
@@ -0,0 +1,3 @@
+﻿    String s = "Hello       World and            balalo-aloha";
+    String s1 = s.replaceAll("\\s+", " ");
+    System.out.println(s1);
---
+++ bool.py
@@ -0,0 +1,6 @@
+﻿dat = 1
+if bool(dat):
+	print(dat, ' is Правда')
+else:
+        print(dat, ' is Ложь')
+	
